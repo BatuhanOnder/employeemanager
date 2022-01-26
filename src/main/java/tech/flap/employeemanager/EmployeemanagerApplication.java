9e75ce1007bce1f9b4cd213bad9e2b8b7694a973
Batuhan Onder <batuhanonderr@gmail.com>
2022-01-26 16:04:04 +0300

    Initial commit

---
+++ src/main/java/tech/flap/employeemanager/EmployeemanagerApplication.java
@@ -0,0 +1,14 @@
+package tech.flap.employeemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeemanagerApplication {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(EmployeemanagerApplication.class, args);
+	}
+
+}
