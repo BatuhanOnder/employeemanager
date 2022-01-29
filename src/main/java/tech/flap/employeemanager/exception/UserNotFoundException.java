2fa6d5a01879995b35224c2e545c33ad8f814271
Batuhan Onder <batuhanonderr@gmail.com>
2022-01-29 15:08:44 +0300

    Added controller, service, repo

---
+++ src/main/java/tech/flap/employeemanager/exception/UserNotFoundException.java
@@ -0,0 +1,8 @@
+package tech.flap.employeemanager.exception;
+
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(String message) {
+        super(message);
+
+    }
+}
